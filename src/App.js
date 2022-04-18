70aaf2c3ff06724d99bbb4557e16f29a37f231c2
Rehanmunir <rehanir.munir@gmail.com>
2022-04-18 12:01:12 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <nylas-mailbox id="a1423dfa-a277-4ab2-8f3a-28a471de298b" > </nylas-mailbox>
+    </div>
+  );
+}
+
+export default App;
